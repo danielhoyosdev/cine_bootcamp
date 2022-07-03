201bb3dd29ee18dad9240f379fa0f618b29d805b
Daniel Hoyos <danielhoyos350@gmail.com>
2022-07-03 00:35:11 -0500

    Creación de los Modelos de Director y Pelicula y su registro en ADMIN.PY

--- peliculas/models.py
+++ peliculas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Director(models.Model):
+    nombre = models.CharField(max_length=50, null=False)
+    apellidos = models.CharField(max_length=50, null=False)
+    fecha_nacimiento = models.DateField(null=False)
+    fecha_fallecimiento = models.DateField(blank=True, null=True)
+
+
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=50, null=False)
+    snopsis = models.TextField(max_length=200, null=True)
+    lanzamiento = models.DateField(null=False)
+    duracion = models.PositiveIntegerField(null=False, help_text='Duración en Minutos')
+    director = models.ManyToManyField(Director)
